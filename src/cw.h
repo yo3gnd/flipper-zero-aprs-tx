45c8eae59eab33468f20ead7ae1713c702a000c6
YO3GND Richard <dev.9384@yo3gnd.ro>
2025-12-26 14:18:44 +0200

    Zawinski's CW generator. At least the byte packing and shifting is satisfying

---
+++ src/cw.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#define CW_INVALID 0xFFu
+
+extern const uint8_t cw_ascii[127];
+
+uint8_t cw(char c);
+
+/* expects uint8_t cw_char already loaded with cw() */
+#define FOR_EACH_CW_SYMBOL(var) \
+    for (; cw_char > 1; cw_char >>= 1) \
+        for (bool var = ((cw_char & 1u) != 0), _cw_once = true; _cw_once; _cw_once = false)
